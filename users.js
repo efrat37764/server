a6fedf96119845ccb54f40ce7d8fd57cb368d249
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-07-16 00:07:48 +0300

    create users array

---
+++ users.js
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        id: 1,
+        username: "israel_israeli",
+        email: "israel@example.com",
+        password: "password123",
+        borrowedBooks: [101]
+    },
+    {
+        id: 2,
+        username: "dana_levy",
+        email: "dana@example.com",
+        password: "securepassword456",
+        borrowedBooks: []
+    },
+    {
+        id: 3,
+        username: "yossi_cohen",
+        email: "yossi@example.com",
+        password: "mypassword789",
+        borrowedBooks: [102, 105]
+    },
+    {
+        id: 4,
+        username: "noa_shteren",
+        email: "noa@example.com",
+        password: "noapassword111",
+        borrowedBooks: []
+    },
+    {
+        id: 5,
+        username: "avi_levi",
+        email: "avi@example.com",
+        password: "avipassword222",
+        borrowedBooks: [108]
+    }
+];
