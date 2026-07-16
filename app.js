fac3290376eb47961c6016f7db233cc985ecebf6
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-07-16 19:05:23 +0300

    Adding printing of every request that arrives to the server in development mode

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import indexRoutes from './routes/index.route.js';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api', indexRoutes);
 
