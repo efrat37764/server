7a38df4c626ebd2960c4abbcab495dbb651fefa2
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-07-16 19:38:40 +0300

    Adding middleware for security and limiting the number of requests (helmet, rate-limit)

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import helmet from "helmet";
+import rateLimit from 'express-rate-limit';
 import indexRoutes from './routes/index.route.js';
 
 const app = express();
 
+// הוספת כותרות אבטחה להגנה מפני פגיעויות נפוצות ברשת
+app.use(helmet());
+
 app.use(cors());
 
-app.use(express.json());
-
 app.use(morgan('dev'));
+
+// (ההגבלה כאן: 100 בקשות לכל 15 דקות) הגבלת כמות הבקשות מכתובת בודדת כדי למנוע עומס חריג על השרת
+app.use(rateLimit({windowMs: 15 * 60 * 1000, max: 100, message: 'Too many requests from this IP, please try again after 15 minutes',}));
+
+app.use(express.json());
 
 app.use('/api', indexRoutes);
 
