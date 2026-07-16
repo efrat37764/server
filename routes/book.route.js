551d218726bc80f022f3db0ec7aa2cbcb89eb46c
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-07-17 01:31:39 +0300

    implement validation middleware for all route

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getAllBooks, getBook, addBook, updateBook, borrowBook, returnBook, deleteBook } from '../controllers/book.controller.js';
-import { logGetRequests } from '../middlewares/index.middleware.js';
+import { logGetRequests, validate } from '../middlewares/index.middleware.js';
+import { bookSchema } from '../schemas/joiSchemas.js';
 
 const router = Router();
 
 router.get('/', logGetRequests, getAllBooks);
 router.get('/:id', logGetRequests, getBook);
-router.post('/', addBook);
-router.put('/:id', updateBook);
+router.post('/', validate(bookSchema), addBook);
+router.put('/:id', validate(bookSchema), updateBook);
 router.patch('/:id/borrow', borrowBook);
 router.patch('/:id/return', returnBook);
 router.delete('/:id', deleteBook);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { sign_up, sign_in, getAllUsers } from "../controllers/user.controller.js";
-import { logGetRequests } from '../middlewares/index.middleware.js';
+import { logGetRequests, validate } from '../middlewares/index.middleware.js';
+import { userSchema, loginSchema } from '../schemas/joiSchemas.js';
 
 
 const router = Router();
 
-router.post('/', sign_up);
-router.post('/sign-in', sign_in);
+router.post('/', validate(userSchema), sign_up);
+router.post('/sign-in', validate(loginSchema), sign_in);
 router.get('/', logGetRequests, getAllUsers);
 
 export default router;
