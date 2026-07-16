aff1f28467883b8f874cf148e094c1717ad4ac73
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-07-17 00:11:45 +0300

    Change how the error message is sent (to an object)

--- middlewares/errors.middleware.js
+++ middlewares/errors.middleware.js
@@ -1,7 +1,9 @@
 export const notFound = (req, res, next) => {
-    const error = new Error('Route not found');
-    error.status = 404;
-    next(error);
+    next({
+        status: 404,
+        error: new Error('Route not found'),
+        type: 'Not Found'
+    });
 };
 
 
