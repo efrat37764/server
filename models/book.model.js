d753b57d07d390c5775c3dd62aff500fa01e099b
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-07-22 15:51:07 +0300

    adding validation checks to a book's schema

--- models/book.model.js
+++ models/book.model.js
@@ -1,14 +1,23 @@
 import { model, Schema } from "mongoose";
 
 const bookSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        unique: true,
+        minlength: 2,
+        maxlength: 20
+    },
     price: Number,
-    category: [String],
+    category: {
+        type: [String],
+        enum: ['Adults', 'youth', 'Children', 'Science', 'History', 'Comics']
+    },
     author: {
         name: String,
         phone: String,
         email: String,
     },
 });
+
 
 export const Book = model('books', bookSchema);
